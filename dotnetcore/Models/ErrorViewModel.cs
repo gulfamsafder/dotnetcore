3ee055a0f6255a67fa24134c3236185d845c6809
gulfamsafdar <153251405+gulfamsafdar@users.noreply.github.com>
2024-10-07 10:12:43 +0500

    Add project files.

---
+++ dotnetcore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnetcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
